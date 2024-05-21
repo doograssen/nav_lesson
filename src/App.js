ff476723108ec6dc9a0339224e0371a5cd5b19da
Alexander <rythmag@gmail.com>
2024-05-21 10:25:46 +0300

    exercise

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+export const App = () => {
+	return (
+		<div className="app">
+			<main className="app-main">
+				<h1>Навигация</h1>
+			</main>
+		</div>
+	);
+};
